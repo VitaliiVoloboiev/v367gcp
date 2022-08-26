ebf02e4bc7c866c17d26fa802798797e97e35a02
Vitalii <83081678+VitaliiVoloboiev@users.noreply.github.com>
2022-08-26 12:21:50 +0300

    Create v367.tf

---
+++ v367.tf
@@ -0,0 +1,24 @@
+
+provider "m3" {}
+resource "m3_instance" "my-server" {
+  image = "Ubuntu20.04_64-bit"
+  instance_name = "v367fromGH
+  region_name = var.regionName
+  tenant_name = var.tenantName
+  shape = "MICRO"
+  key_name = var.key_name
+  owner = var.owner
+}
+
+variable "tenantName" {
+  description = "m3_3ah_::)Tenant name"
+}
+variable "regionName" {
+  description = "m3_2ah_::)Region name"
+}
+variable "key_name" {
+  description = "key_name"
+}
+variable "owner" {
+  description = "owner"
+}
